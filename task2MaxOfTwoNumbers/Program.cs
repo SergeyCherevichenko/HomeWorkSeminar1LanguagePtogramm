6142632bd5a2becbbb40fe7d7e03929d5b25191b
SergeyCherevichenko <cherevichenkosn@gmail.com>
2023-07-06 12:01:33 +0500

    Найти max и min из двух введеных чисел

---
+++ task2MaxOfTwoNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numberX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberY = Convert.ToInt32(Console.ReadLine());
+if(numberX > numberY)
+{
+    Console.WriteLine("Это число большее из двух вами введеных: " + " max = " + numberX);
+    Console.WriteLine("Это число меньшее из двух вами введенных: " + " min = " + numberY);
+}
+else if(numberY > numberX)
+{
+    Console.WriteLine("Это число большее из двух вами введенных: " + " max = " +numberY);
+    Console.WriteLine("Это число меньшее из двух вами введенных: " + " min = " + numberX);
+
+}
+else
+{
+    Console.WriteLine("Ваши числа равны: " + numberX + " = " + numberY);
+}
