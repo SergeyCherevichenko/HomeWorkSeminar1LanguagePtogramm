bcc2fea40224fb192d0a78daa05325c07394f3eb
SergeyCherevichenko <cherevichenkosn@gmail.com>
2023-07-06 12:37:10 +0500

    Четное или не четное введеное число

---
+++ task6EvenNumber/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите ваше число: ");
+int numberuser = Convert.ToInt32(Console.ReadLine());
+if (numberuser % 2 == 0)
+{
+    Console.WriteLine("ваше число: " + numberuser + " - четное");
+}
+else 
+{
+    Console.WriteLine("Ваше число: " + numberuser + " - не четное");
+}
