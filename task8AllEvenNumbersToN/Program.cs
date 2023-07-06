1259398ad2d7d19a15ac88f8714114ce0bcfbd59
SergeyCherevichenko <cherevichenkosn@gmail.com>
2023-07-06 21:42:46 +0500

    Все  четные числа от 0 до N (введено пользователем)

---
+++ task8AllEvenNumbersToN/Program.cs
@@ -0,0 +1,27 @@
+﻿int EnterNumber()
+{
+    Console.WriteLine("Введите число: ");
+    int numberuser = Convert.ToInt32(Console.ReadLine());
+    return numberuser;
+} 
+int x = EnterNumber();  
+    while (x <= 0)
+{
+    Console.WriteLine("Вы ввели число менее либо равное 0! Попробуйте ввести еще раз!");
+    x = EnterNumber();
+}
+
+   int i = 1;
+   Console.Write("Ваши четные числа  от 0 до " + x + " :   ");
+while(i < x + 1 )
+{
+    if(i % 2 == 0)
+    Console.Write(i + " ");
+    i++;
+    }   
+
+
+
+
+
+
